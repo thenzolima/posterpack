db436bccfedbf5163ada0a89c2d7f1af3afb5287
Enzo Lima <129903213+thenzolima@users.noreply.github.com>
2023-05-06 01:06:41 -0300

    update

---
+++ script.js
@@ -0,0 +1,31 @@
+const myAtropos = Atropos({
+    el: '.my-atropos',
+    shadow: false,
+  });
+
+  const mySecondAtropos = Atropos({
+    el: '.my-second-atropos',
+    shadow: false,
+  });
+
+  const myThirdAtropos = Atropos({
+    el: '.my-third-atropos',
+    shadow: false,
+  });
+
+  const myFourthAtropos = Atropos({
+    el: '.my-fourth-atropos',
+    shadow: false,
+  });
+
+  const myFifthAtropos = Atropos({
+    el: '.my-fifth-atropos',
+    shadow: false,
+  });
+
+
+  const mySixthAtropos = Atropos({
+    el: '.my-sixth-atropos',
+    shadow: false,
+  });
+
